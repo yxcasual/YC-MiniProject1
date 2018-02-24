ac41dff94accc9bba43beceb99ea083b03217dde
ychakola <yc566@njit.edu>
2018-02-24 13:16:44 -0500

    mini project basic setup:2

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <div class="card mt-3 pl-2 pr-2">
+        <div class="card-title"><h1>Contact</h1>
+            <p class="lead">Please use this site for contact</p></div>
+
+        <div class="card-body">
+
+            <form action="/contact" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="body">Message</label>
+                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+Route::get('/about', function () {
+    return view('pages.about');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+Route::post('/contact', function () {
+
+
+   // dd(request());
+
+    $data = request()->all();
+
+    //dd($data);
+
+    echo "Email: " .  $data['email'] . '<br>';
+    echo "Body: " . $data['body'];
+
+});
+Route::get('/logout', function () {
+    return view('pages.logout');
+});
+Route::get('/signin', function () {
+    return view('pages.signin');
+});
+Route::get('/signup', function () {
+    return view('pages.signup');
+});
+Route::get('/viewmessages', function () {
+    return view('pages.viewmessages');
+});
+
