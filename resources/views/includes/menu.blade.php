b2515e9cc6b2bb5144da8b4f23085b332ffd6bae
ychakola <yc566@njit.edu>
2018-02-24 15:36:46 -0500

    Menu/Nav bar done

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-<a class="navbar-brand" href="#">Navbar</a>
+
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
@@ -16,27 +16,15 @@
             <a class="nav-link" href="/contact">Contact</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/signin">Sign In</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/signup">Sign Up</a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link" href="/viewmessages">View Messages</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/logout">Logout</a>
-        </li>
-
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>
     </ul>
+    <li class="nav-item">
+        <a class="nav-link" href="/signin">Sign In</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/signup">Sign Up</a>
+    </li>
     <ul class="nav navbar-nav navbar-right">
         <li class="nav-item">
             <a class="nav-link" href="/logout">Logout</a>
