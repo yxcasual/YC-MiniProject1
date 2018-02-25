2db7fbd3249b2a22c15a13a6a9e2066e2d4169ca
ychakola <yc566@njit.edu>
2018-02-24 19:39:38 -0500

    Modifications to home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,10 +9,13 @@
         <!-- Main jumbotron for a primary marketing message or call to action -->
         <div class="jumbotron">
             <div class="container">
+                <img src="/css/images/andromeda.jpg" class="img-fluid" alt="Responsive image">
 
-                <h1 class="display-3">Mission to Andromeda</h1>
-                <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+                <h1 class="display-3">The Andromeda Galaxy</h1>
+                <p>The Andromeda Galaxy also known as Messier 31, M31, or NGC 224, is a spiral galaxy approximately 780
+                    kiloparsecs (2.5 million light-years) from Earth, and the nearest major galaxy to the Milky Way.
+                    Its name stems from the area of the sky in which it appears, the constellation of Andromeda.</p>
+
             </div>
         </div>
 
@@ -20,19 +23,43 @@
             <!-- Example row of columns -->
             <div class="row">
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Observation History</h2>
+                    <p>Around the year 964, the Persian astronomer Abd al-Rahman al-Sufi described the Andromeda Galaxy,
+                        in his Book of Fixed Stars as a "nebulous smear". Star charts of that period labeled it as the Little Cloud.
+                        In 1612, the German astronomer Simon Marius gave an early description of the Andromeda Galaxy based on
+                        telescopic observations. The German philosopher Immanuel Kant in 1755 in his work Universal Natural
+                        History and Theory of the Heavens conjectured that the blurry spot was an island universe. In 1764,
+                        Charles Messier cataloged Andromeda as object M31 and incorrectly credited Marius as the discoverer
+                        despite it being visible to the naked eye. In 1785, the astronomer William Herschel noted a faint
+                        reddish hue in the core region of Andromeda. He believed Andromeda to be the nearest of all the
+                        "great nebulae", and based on the color and magnitude of the nebula, he incorrectly guessed that
+                        it is no more than 2,000 times the distance of Sirius. In 1850, William Parsons, 3rd Earl of Rosse,
+                        saw and made the first drawing of Andromeda's spiral structure. </p>
+                    <p><a class="btn btn-secondary" href="https://en.wikipedia.org/wiki/Andromeda_Galaxy" role="button">View details &raquo;</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Collision with milky way</h2>
+                    <p> The Andromeda Galaxy is approaching the Milky Way at about 110 kilometres per second (68 mi/s).
+                        It has been measured approaching relative to our Sun at around 300 kilometres per second (190 mi/s)
+                        as the Sun orbits around the center of our galaxy at a speed of approximately 225 kilometres per
+                        second (140 mi/s). This makes the Andromeda Galaxy one of about 100 observable blueshifted galaxies.
+                        Andromeda Galaxy's tangential or sideways velocity with respect to the Milky Way is relatively much
+                        smaller than the approaching velocity and therefore it is expected to collide directly with the Milky
+                        Way in about 4 billion years. A likely outcome of the collision is that the galaxies will merge to
+                        form a giant elliptical galaxy[97] or perhaps even a large disc galaxy.Such events are frequent among
+                        the galaxies in galaxy groups. The fate of the Earth and the Solar System in the event of a collision
+                        is currently unknown. Before the galaxies merge, there is a small chance that the Solar System could be
+                        ejected from the Milky Way or join the Andromeda Galaxy.</p>
+                    <p><a class="btn btn-secondary" href="https://en.wikipedia.org/wiki/Andromeda_Galaxy" role="button">View details &raquo;</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Interactions</h2>
+                    <p>New images from NASA's Spitzer Space Telescope shed light on the Andromeda Galaxy's violent past.
+                        The images show that one of the latter's satellite galaxies, M32, blasted through one of the Andromeda
+                        Galaxy's spiral arms a few million years ago. Infrared pictures of the galaxy's two spiral arms demonstrate
+                        that they and the prominent star-forming ring are separate structures. The images also show a hole where the
+                        rings seem to split into arcs. This hole is where astronomers believe M32 punched through Andromeda's galactic disk.</p>
+                    <p><a class="btn btn-secondary" href="https://en.wikipedia.org/wiki/Andromeda_Galaxy" role="button">View details &raquo;</a></p>
                 </div>
             </div>
 
